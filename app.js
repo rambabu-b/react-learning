4652a4261a31d7f2298fb963394753089b3b0af1
Ram <rambabu.b@quiddityinfotech.com>
2025-01-28 13:33:05 +0530

    practice

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement(
 //     'h1',
 //     {id:'main-header'},
 //     'helo from react'
 // );
+
+//using react
 const parent = React.createElement("div", { id: "parent" }, [
   [
     React.createElement("div", { id: "child" }, [
@@ -15,8 +19,22 @@ const parent = React.createElement("div", { id: "parent" }, [
         React.createElement("h2", { id: "iam-h2" }, "i am h2q"),
       ]),
     ],
-  ],
+  ]
 ]);
-
+const TitleComponent = () => (
+  <div>
+    {parent}
+    <span> small heading</span>
+  </div>
+);
+// using jxs it a html like syntax.
+//babel is the one who converts the jsx into react code to understand by js engine.
+const HeadingComponent = () => (
+  <>
+    <h1 id="main-header">react learning </h1>
+    <TitleComponent />
+  </>
+);
+//console.log(jsxHeading)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
